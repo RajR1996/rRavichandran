5a9ac5c75599add710fc188adc332ab58ce6c6c4
RajR1996 <b4025068@my.shu.ac.uk>
2019-04-13 16:03:49 +0100

    Draft version of Home, About and Contact created, css file added

--- itservices/reviewApp/views.py
+++ itservices/reviewApp/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+	return render(request, 'reviewApp/home.html', {'title': 'Home'})
+
+def about(request):
+	return render(request, 'reviewApp/about.html', {'title': 'About Us'})
+
+def contact(request):
+	return render(request, 'reviewApp/contact.html', {'title': 'Contact Us'})
+
+def product(request):
+	all_products = {
+		'products': products
+	}
+	return render(request, 'reviewApp/product.html', all_products)
