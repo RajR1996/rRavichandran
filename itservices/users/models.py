3d86facf96f9a1b38d85d63526019400f30a6760
RajR1996 <b4025068@my.shu.ac.uk>
2019-04-16 21:02:53 +0100

    User profile page displaying all details defined in model Profile

--- itservices/users/models.py
+++ itservices/users/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Profile(models.Model):
 	user = models.OneToOneField(User, on_delete = models.CASCADE)
 	fullname = models.CharField(max_length=100, null=True)
 	dob = models.DateField(null=True)
 	address = models.TextField(null=True)
 	city = models.CharField(max_length=100, null=True)
+	country = models.CharField(max_length=100, null=True)
 	profilephoto = models.ImageField(default='profiles/default_profile.jpg', upload_to='profiles')
 
 def __str__(self):
