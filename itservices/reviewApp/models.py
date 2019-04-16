974a72273939e3b124df468263acf83c7e1f0ca8
RajR1996 <b4025068@my.shu.ac.uk>
2019-04-16 15:18:45 +0100

    Profile and Review, Product was completed earlier on, models created and migrations correctly implemented

--- itservices/reviewApp/models.py
+++ itservices/reviewApp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from users.models import Profile
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator, MaxValueValidator
 
@@ -12,14 +13,15 @@ class Product(models.Model):
 	description = models.TextField()
 	productphoto = models.ImageField(default='default_product.jpg', upload_to='reviewApp/static/product_images')
 
-# class Review(models.Model):
-# 	product = models.ForeignKey(Product)
-# 	profile = models.ForeignKey(Profile)
-# 	author = models.ForeignKey(User, on_delete=models.CASCADE)
-# 	rating = model.PositiveSmallIntegerField(default=1, validators = [MinValueValidator(1), MaxValueValidator(5)])
-# 	reviewtext = models.TextField()
-# 	postdate = models.DateTimeField(auto_now_add=True)
-# 	lastmodified = models.DateTimeField(auto_now=True)
+class Review(models.Model):
+	product = models.ForeignKey(Product, on_delete=models.CASCADE)
+	profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	author = models.CharField(max_length=100)
+	rating = models.PositiveSmallIntegerField(default=1, validators = [MinValueValidator(1), MaxValueValidator(5)])
+	reviewtext = models.TextField()
+	postdate = models.DateTimeField(auto_now_add=True)
+	lastmodified = models.DateTimeField(auto_now=True)
 
 def __str__(self):
 	return self.type
--- itservices/users/models.py
+++ itservices/users/models.py
@@ -1,11 +1,13 @@
 from django.db import models
-# from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 
-# class Profile(models.Model):
-# 	user = models.OneToOneField(User, on_delete = models.CASCADE)
-# 	fullname = models.CharField(max_length=100)
-# 	dob = models.DateField()
-# 	address = models.TextField()
-# 	city = models.CharField(max_length=100)
-# 	profilephoto = models.ImageField(default='default_profile.jpg', upload_to='reviewApp/static/profile_images')
+class Profile(models.Model):
+	user = models.OneToOneField(User, on_delete = models.CASCADE)
+	fullname = models.CharField(max_length=100)
+	dob = models.DateField()
+	address = models.TextField()
+	city = models.CharField(max_length=100)
+	profilephoto = models.ImageField(default='default_profile.jpg', upload_to='reviewApp/static/profile_images')
 
+def __str__(self):
+	return f'Profile for {self.user.username}'
