db063a5dd14a211293dff98b4ca54d3e4b3d36a5
RajR1996 <b4025068@my.shu.ac.uk>
2019-04-16 01:28:36 +0100

    Fully functional login and logout, installed Crispy Forms and added it to html pages

--- itservices/users/views.py
+++ itservices/users/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.forms import UserCreationForm
+from .forms import UserRegisterForm
 from django.contrib import messages
 
 def register(request):
 	if request.method == 'POST':
-		form = UserCreationForm(request.POST)
+		#form = UserCreationForm(request.POST)
+		form = UserRegisterForm(request.POST)
 		if form.is_valid():
 			form.save()
 			username = form.cleaned_data.get('username')
-			messages.success(request, f'Account created for {username}!')
-			return redirect('reviewApp-home')
+			messages.success(request, f'Account created for {username}! You can now login to Review App!')
+			return redirect('login')
 	else:
-		form = UserCreationForm()
+		#form = UserCreationForm()
+		form = UserRegisterForm()
 	return render(request, 'users/register.html', {'form':form})
